7579085406ee2f912cec1582142d230cd606e1b0
Tanmayee Chalamalasetti <tanmayeesc@Tanmayees-MacBook-Pro-190.local>
2025-10-25 19:33:00 -0700

    Task 1 completed

--- Goat.h
+++ Goat.h
@@ -6,23 +6,28 @@
 using namespace std;
 
 class Goat {
-private:
-    string name;
-    int age;
-    string color;
-public: 
-    Goat()                          { name = ""; age = 0; color = ""; }
-    // write three more constructors
+ private:
+  string name;
+  int age;
+  string color;
 
-    // setters and getters
-    void set_name(string n)         { name = n; };
-    string get_name() const         { return name; };
-    void set_age(int a)             { age = a; };
-    int get_age() const             { return age; }
-    void set_color(string c)        { color = c; }
-    string get_color() const        { return color; }
+ public:
+  Goat() {
+    name = "";
+    age = 0;
+    color = "";
+  }
+  // write three more constructors
 
-    // write overloaded < operator for the std::list
+  // setters and getters
+  void set_name(string n) { name = n; };
+  string get_name() const { return name; };
+  void set_age(int a) { age = a; };
+  int get_age() const { return age; }
+  void set_color(string c) { color = c; }
+  string get_color() const { return color; }
+
+  // write overloaded < operator for the std::list
 };
 
 #endif
